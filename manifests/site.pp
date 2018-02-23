7c5630c8338ecc610b830b96294ad8b1a2ea81b3
Ihar Vauchok <napa93@mail.ru>
2018-02-23 11:00:48 +0000

    Test2/changed site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a some text',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner => 'root',
-  }
 }
 
