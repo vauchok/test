fd9b58139980f54085f298e88745f70ed80d17ec
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-02-26 13:51:47 +0200

    Add/java

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,8 +2,22 @@ class minecraft (
   $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   $minecraft_dir = '/opt/minecraft',
 ){
+  file {'/tmp/epel-release-latest-7.noarch.rpm':
+    ensure => file,
+    source => https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm,
+  }
+  package {'epel-release-7':
+    provider => 'rpm',
+    install_options => ['-ivh'],
+    ensure => 'present',
+    source => '/tmp/epel-release-latest-7.noarch.rpm',
+    before => Package['java']
+  }
   file {$minecraft_dir:
     ensure => directory,
+  }
+  package {'java':
+    ensure => present,
   }
   file {"${minecraft_dir}/minecraft_server.1.12.2.jar":
     ensure => file,
