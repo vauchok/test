c625a63c904a46146dbe260c5a9bbb7c72333c18
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-02-26 11:34:36 +0200

    Add/variables block to init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
-class minecraft {
-  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar'
-  $minecraft_dir = '/opt/minecraft'
+class minecraft (
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  $minecraft_dir = '/opt/minecraft',
+){
   file {$minecraft_dir:
     ensure => directory,
   }
