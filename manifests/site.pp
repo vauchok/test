170392af374d492e215661034c04ec1380b91f08
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-03-03 13:51:00 +0200

    include zabbix

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,13 @@
 node 'puppet2' {
   hiera_include(classes)
+  class { 'apache':
+    mpm_module => 'prefork',
+  }
+  include apache::mod::php
+
+  class { 'postgresql::server': }
+
+  class { 'zabbix':
+    zabbix_url    => 'puppet2',
+  }
 }
