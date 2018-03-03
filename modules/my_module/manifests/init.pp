ab65e526b85c23fdb64cd17d5bb5449dd17f9000
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-03-03 13:04:13 +0200

    create module

--- modules/my_module/manifests/init.pp
+++ modules/my_module/manifests/init.pp
@@ -1,1 +1,13 @@
-#test
+class my_module (
+  $env_param = $settings::environment,
+  $msg1_param = "msg1",
+  $msg2_param = "msg2"
+) {
+  file { 'content file':
+    path => '/tmp/content.txt',
+    ensure => file,
+    content => "${env_param}\n${msg1_param}\n${msg2_param}\n"
+  }
+  $users = hiera('users')
+  create_resources(user, $users, {})
+}
