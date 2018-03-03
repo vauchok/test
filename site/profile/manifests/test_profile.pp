f404e7dcc13a6817044ed5b67ce39dde9229044e
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-03-03 13:06:28 +0200

    Feature

--- site/profile/manifests/test_profile.pp
+++ site/profile/manifests/test_profile.pp
@@ -1,13 +1,3 @@
-class profile::test_profile (
-  $env_param = $settings::environment,
-  $msg1_param = "msg1",
-  $msg2_param = "msg2"
-) {
-  file { 'content file':
-    path => '/tmp/content.txt',
-    ensure => file,
-    content => "${env_param}\n${msg1_param}\n${msg2_param}\n"
-  }
-  $users = hiera('users')
-  create_resources(user, $users, {})
+class profile::test_profile {
+  include my_module
 }
