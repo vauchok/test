98a1a3a2a0996e3a373a4a435dd5ac00a1a40bc1
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-03-03 13:16:12 +0200

    Feature

--- modules/my_module/manifests/init.pp
+++ modules/my_module/manifests/init.pp
@@ -1,13 +1,14 @@
 class my_module (
   $env_param = $settings::environment,
-  $msg1_param = "msg1",
-  $msg2_param = "msg2"
+  $msg1_param = hiera('msg1_param'),
+  $msg2_param = hiera('msg1_param1')
 ) {
   file { 'content file':
     path => '/tmp/content.txt',
     ensure => file,
     content => "${env_param}\n${msg1_param}\n${msg2_param}\n"
   }
+  notify { "var1: $msg1_param, var2: $msg2_param":}
   $users = hiera('users')
   create_resources(user, $users, {})
 }
